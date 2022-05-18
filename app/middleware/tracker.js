d5ed143a9845efdc89566ef65b03040145e28b97
jsonma <775087367@qq.com>
2022-05-18 09:18:01 +0800

    fix: add polyfill to uuid generator

--- app/middleware/tracker.js
+++ app/middleware/tracker.js
@@ -1,9 +1,15 @@
-'use strict';
-const { v1: uuidv1 } = require('uuid');
+'use strict'
+const { v1: uuidv1 } = require('uuid')
 
-module.exports = options => {
+module.exports = (options) => {
+  function getPolyfillUUID() {
+    const uuid = uuidv1()
+    if (String.prototype.replaceAll) return uuid.replaceAll('-', '')
+    return uuid.split('-').join('')
+  }
+
   function idGenerator(format) {
-    return format === 'random' ? uuidv1().replaceAll('-', '') : uuidv1();
+    return format === 'random' ? getPolyfillUUID() : uuidv1()
   }
 
   return async function tracker(ctx, next) {
@@ -12,18 +18,18 @@ module.exports = options => {
       format = 'random',
       autoGenerateSpanId = true,
       key = 'tracker',
-    } = options;
-    const { headers } = ctx;
-    const spanId = autoGenerateSpanId && idGenerator(format);
-    const traceId = headers['trace-id'] || headers.traceId || idGenerator(format);
-    let parentSpanId = headers['span-id'] || headers.spanID;
-    if (!parentSpanId && autoGenerateParentSpanId) parentSpanId = idGenerator(format);
+    } = options
+    const { headers } = ctx
+    const spanId = autoGenerateSpanId && idGenerator(format)
+    const traceId = headers['trace-id'] || headers.traceId || idGenerator(format)
+    let parentSpanId = headers['span-id'] || headers.spanID
+    if (!parentSpanId && autoGenerateParentSpanId) parentSpanId = idGenerator(format)
     const tracker = {
       traceId,
       ...(spanId ? { spanId } : null),
       ...(parentSpanId ? { parentSpanId } : null),
-    };
-    ctx[key] = ctx.request[key] = ctx.response[key] = tracker;
-    await next();
-  };
-};
+    }
+    ctx[key] = ctx.request[key] = ctx.response[key] = tracker
+    await next()
+  }
+}
